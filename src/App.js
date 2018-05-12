fd428cc6735b878688d89fbfca3182fd1c417b52
Josiah Maina <josiah5115@gmail.com>
2018-05-11 22:44:46 -0600

    updated static default values to dynamic

--- src/App.js
+++ src/App.js
@@ -8,19 +8,33 @@ import Weather from './components/Weather.js';
 const API_KEY = "717d8258895a8589755edb6bc0463525";
 
 class App extends React.Component {
+  
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+    
+
+  }
 
     getWeather = async (e) => {
 
       e.preventDefault();
 
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+      const city = e.target.elements.city.value;
+      const country = e.target.elements.country.value;
+
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
       
       const data = await api_call.json();  // json method efficiently converts api data we receive into a readerble form. 
 
       console.log(data);
 
     }
-    
+
     render() {
       return (
         <div>
